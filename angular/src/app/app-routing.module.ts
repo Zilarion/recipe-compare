be77298f889fbd80fb3202c2b0fdec090f678deb
Ruud Andriessen <ruud.andriessen@gmail.com>
2020-03-28 14:27:57 +0100

    Create shopping view and naive list

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShoppingViewComponent } from './routes/shopping-view/shopping-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'shopping-list', component: ShoppingViewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingViewComponent } from './routes/shopping-view/shopping-view.component';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingViewComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
